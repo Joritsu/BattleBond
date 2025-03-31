6b37c084ccff446c89c7127f410fd49f5b2d09f2
Dominykas Arnauskas <domarn1@ktu.lt>
2025-03-31 14:20:40 +0300

    TrapsAndDoors

---
+++ Assets/Scripts/Spikes/Spikes_trap.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class Spikes_trap : MonoBehaviour
+{
+    [SerializeField] public float spikeSpeed;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void onTrigger(bool state, float waitTime)
+    {
+        if (anim != null)
+        {
+            StartCoroutine(spikeStop(waitTime));
+            StartCoroutine(wait(waitTime, state));
+        }
+    }
+
+    IEnumerator spikeStop(float waitTime)
+    {
+        anim.SetTrigger("TrOpen");
+        yield return new WaitForSeconds(waitTime);
+        anim.SetTrigger("TrClose");
+    }
+    IEnumerator wait(float waitTime, bool state)
+    {
+        yield return new WaitForSeconds(waitTime);
+        yield return state = true;
+    }
+}
